7d4fdf527afd47ee9a2e588a5b7c7403279151ef
syun77 <kenmomomomomo@gmail.com>
2018-12-16 15:06:53 +0900

    Implement the player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float MOVE_SPEED = 5;
+
+    // ======================================
+    // Variables. Objects.
+    Rigidbody2D _rigidbody2D;
+
+	// Use this for initialization
+	void Start () {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector2 v = new Vector2(horizontal, vertical);
+        v.Normalize();
+        v *= MOVE_SPEED;
+        _rigidbody2D.velocity = v;
+	}
+}
