4e6a0456f1cba73152749078249c54a2fc1e819d
syun77 <kenmomomomomo@gmail.com>
2018-12-18 13:25:05 +0900

    Implement the UI

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
     public float MOVE_SPEED;
     public float REFLECT_OFFSET;
+    public int MAX_POWER;
 
     // ======================================
     // Variables. Objects.
     public GameObject _shot;
     public GameObject _horming;
+    public Text _textShot;
+    public Text _textShield;
     Reflect _reflect;
     Rigidbody2D _rigidbody2D;
 
     // ======================================
     // Variables.
     bool _enableShot = false;
+    int _powerShot = 100;
+    int _powerShield = 100;
 
     // ======================================
     // Functions.
@@ -39,6 +45,8 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Move();
+
+        _UpdateUI();
     }
 
     private void FixedUpdate() {
@@ -83,6 +91,17 @@ public class Player : MonoBehaviour {
     void _UpdateShot() {
 
         _enableShot = Input.GetKey(KeyCode.Z);
+        if(_enableShot == false) {
+            _powerShot += 1;
+            if(_powerShot > MAX_POWER) {
+                _powerShot = MAX_POWER;
+            }
+        }
+
+        if(_powerShot <= 0) {
+            _enableShot = false;
+        }
+
         if (_enableShot) {
             Vector3 v = new Vector3(transform.position.x, transform.position.y);
             GameObject obj = Instantiate(_shot, v, Quaternion.identity);
@@ -90,7 +109,17 @@ public class Player : MonoBehaviour {
             float degree = 90;
             degree += Random.Range(-10, 10);
             shot.SetVelocity(degree, shot.MOVE_SPEED);
+
+            _powerShot -= 3;
+            if(_powerShot < 0) {
+                _powerShot = 0;
+            }
         }
+    }
+
+    void _UpdateUI() {
+        _textShot.text = "Shot: " + _powerShot + "%";
+        _textShield.text = "Shield: " + _powerShield + "%";
     }
 
 
