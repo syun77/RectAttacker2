a0c577b7461b3af165c35f7cb18a4bded4064572
syun77 <kenmomomomomo@gmail.com>
2018-12-19 23:06:30 +0900

    Implement whip bullets

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -41,25 +41,46 @@ public class Enemy : MonoBehaviour {
         _rigidbody2D = GetComponent<Rigidbody2D>();
 	}
 
-    void DoBullet(float degree, float speed) {
+    void DoBullet(float degree, float speed, float msWait=0f) {
+        StartCoroutine(DoBulletCoroutine(degree, speed, msWait));
+    }
+
+    IEnumerator DoBulletCoroutine(float degree, float speed, float msWait) {
+        if(msWait > 0f) {
+            // Wait
+            yield return new WaitForSeconds(msWait * 0.001f);
+        }
+
         Vector3 v = new Vector3(transform.position.x, transform.position.y);
         GameObject obj = Instantiate(bullet, v, Quaternion.identity);
         Bullet b = obj.GetComponent<Bullet>();
         b.SetVelocity(degree, speed);
     }
-    void DoBulletAim(float speed) {
+
+    void DoBulletAim(float speed, float msWait=0f) {
         float aim = Utils.GetAim2D(this.gameObject, _player.gameObject);
-        DoBullet(aim, speed);
+        DoBullet(aim, speed, msWait);
     }
 
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 	}
 
     private void FixedUpdate() {
+
+        {
+            float vx = _rigidbody2D.velocity.x * 0.97f;
+            float vy = _rigidbody2D.velocity.y * 0.97f;
+            _rigidbody2D.velocity = new Vector2(vx, vy);
+        }
+
         interval++;
-        if(interval%10 == 1) {
-            DoBulletAim(5);
+        if(interval%60 == 1) {
+            for (int i = 0; i < 6; i++) {
+                float msWait = 100 * i;
+                DoBulletAim(2 + 0.5f * i, msWait);
+            }
         }
     }
 
